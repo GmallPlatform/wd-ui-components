f6903f5bb48ae43ef5bc7f6652d5ae488c09be93
igorchugurov <igorchugurov@gmail.com>
2025-04-08 16:19:05 +0200

    fix: add InputText

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import "./styles/globals.css";
 export { default as FormWithFieldsCompnent } from "./components/formComponent/FormComponent";
 export { default as InputHTMLEditor } from "./components/inputs/InputHTMLEditor";
+export { InputSelect } from "./components/inputs/Select";
+export { InputDate } from "./components/inputs/InputDate";
+export { InputSwitch } from "./components/inputs/InputSwitch";
+export { InputText } from "./components/inputs/InputText";
