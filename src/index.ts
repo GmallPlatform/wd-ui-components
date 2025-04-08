a67974ea80bc684fe4ddc79553c2a15a8b06a9a4
igorchugurov <igorchugurov@gmail.com>
2025-04-08 16:59:19 +0200

    fix: add InputLabel

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { InputSelect } from "./components/inputs/Select";
 export { InputDate } from "./components/inputs/InputDate";
 export { InputSwitch } from "./components/inputs/InputSwitch";
 export { InputText } from "./components/inputs/InputText";
+export { default as InputLabel } from "./components/inputs/InputLabel";
